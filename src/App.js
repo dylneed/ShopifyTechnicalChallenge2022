fb7586463ee4fa787948071c37d3959f030c3725
Dylan Needleman <dylneed@gmail.com>
2022-01-12 16:08:58 -0500

    Implement Minimum Viable Product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import './App.css';
+//https://docs.google.com/document/d/13zXpyrC2yGxoLXKktxw2VJG2Jw8SdUfliLM-bYQLjqE/edit#
+//https://api.nasa.gov (I quite like EPIC)
+//NASA API Key: qad1lSJeLRQHoMp2bhrgzAfBUJ6e6GbR2egkGlC5
 import Home from './pages/Home.jsx';
+import Liked from './pages/Liked.jsx';
+import React, {useState, useEffect} from 'react';
+import {Page, Tabs} from '@shopify/polaris';
 
 function App() {
+  const [selected, setSelected] = useState(0);
+
+  const [likedImages, setLikedImages] = useState([]);
+
+  const [selectedDate, setSelectedDate] = useState({
+    start: new Date(),
+    end: new Date()
+  });
+
+  
+  useEffect(() => console.log(likedImages), [,likedImages]);
+
+  const tabs = [
+    {
+      id: "Home",
+      content: "Home",
+      accesibilityLabel: "Home",
+      panelId: "Home",
+    },
+    {
+      id: "Liked",
+      content: "Liked Images",
+      accesibilityLabel: "Liked",
+      panelId: "Liked",
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        <Page
+          fullWidth
+          title="Images from EPIC"
+          subtitle="These images were captured by DSCOVR's Earth Polychromatic Imaging Camera"
+        >
+          <Tabs tabs={tabs} selected={selected} onSelect={setSelected}>
+            {tabs[selected].id === "Home" && (
+              <Home
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
+                likedImages={likedImages}
+                setLikedImages={setLikedImages}
+              />
+            )}
+            {tabs[selected].id === "Liked" && (
+              <Liked
+                likedImages={likedImages}
+                setLikedImages={setLikedImages}
+              />
+            )}
+          </Tabs>
+        </Page>
       </header>
     </div>
   );
--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -1,51 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import {DatePicker, MediaCard, Layout, Spinner} from '@shopify/polaris';
+import {MediaCard, Layout, Spinner} from '@shopify/polaris';
 let api_key = "qad1lSJeLRQHoMp2bhrgzAfBUJ6e6GbR2egkGlC5";
 
-function Home({selectedDate, setSelectedDate, likedImages, setLikedImages}) {
-  const [images, setImages] = useState();
-
-  const [{month, year}, setDisplayDate] = useState({
-    month: selectedDate.start.getMonth(), 
-    year: selectedDate.start.getFullYear(),
-  })
-
-  const fetchImages = async () => {
-    try {
-      setImages();
-      let dateString = selectedDate.start.toISOString().split("T")[0]
-      let url = "https://api.nasa.gov/EPIC/api/enhanced/date/" + dateString + "?api_key=" + api_key;
-      fetch(url, {method:"GET"})
-        .then(response => {return response.json();})
-        .then(data => {setImages(data)})
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => fetchImages(), [,selectedDate]);
-  useEffect(() => console.log(images),[,images]);
-
+function Liked({likedImages, setLikedImages}) {
   return (
     <>
-      <br />
-      {images && images.length === 0 ? (<p>No data has been reported for this day</p>) : <br />}
+      <br/>
+      {likedImages && likedImages.length === 0 ? (<p>You have not liked any photos yet</p>) : <br />}
       <Layout>
-        <Layout.Section oneThird/>
-        <Layout.Section oneThird>
-          <DatePicker
-            month={month}
-            year={year}
-            selected={selectedDate}
-            onChange={(day) => setSelectedDate(day)}
-            onMonthChange={(m, y) => setDisplayDate({month: m, year: y})}
-          />
-        </Layout.Section>
-        <Layout.Section oneThird/>
-      </Layout>
-      <br />
-      <Layout>
-        {images ? images.map((image) => (
+        {likedImages ? likedImages.map((image) => (
           <Layout.Section oneHalf>
             <MediaCard
               title={image.image}
@@ -91,4 +54,4 @@ function Home({selectedDate, setSelectedDate, likedImages, setLikedImages}) {
   );
 }
 
-export default Home;
+export default Liked;
